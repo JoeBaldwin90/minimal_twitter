216af356bb325601ca4ae7dea080559398056bc8
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-02 22:41:33 +0100

    DB fix

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,8 @@ class TweetsController < ApplicationController
 
     if session[:token].present?
       client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = Rails.application.credentials.twitter_token
-        config.consumer_secret     = Rails.application.credentials.twitter_token_secret
+        config.consumer_key        = 'OuyD2ohfHuUDfpA0BJoVNj2fL'
+        config.consumer_secret     = 'uJbKjucwV0kEaiMaskpWOsOmlOM62ArRquH9dxaDG2T7by6zii'
         config.access_token        = session[:token]
         config.access_token_secret = session[:token_secret]
       end
